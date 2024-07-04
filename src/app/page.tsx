551dc85a3de339778a65acc94e5ec1f52f068e8d
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-07-05 02:30:53 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ return (
 </div>
 
 <div className='flex justify-center'>
-    <button onClick={()=>startroulette()} className={`duration-200 ${mustSpin ? '' : 'hover:scale-95'}`}>
+    <button onClick={()=>startroulette()} className={`scale-95 duration-200 ${mustSpin ? '' : 'hover:scale-100'}`}>
 
 
 
